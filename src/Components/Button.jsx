bbcc47f2d5a8bf7b2d4437d44c6ab77f2e5226c7
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-16 14:46:25 +0530

    css hover

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import styled from "styled-components";
-import "./Button.css";
+// import "./Button.css";
 
-export const Button = ({ children, onClick }) => {
-  return (
-    <button onClick={onClick} className="mybutton">
-      {children}{" "}
-    </button>
-  );
-};
+const Button = styled.button`
+  border: none;
+  background-color: blue;
+  border-radius: 4px;
+  padding: 10px;
+  transition: all 0.4s 0s;
+  margin: 5px;
+  color: white;
+
+  &:hover {
+    color: white;
+    box-shadow: -3px 6px 2px 1px rgba(0, 0, 255, 0.2);
+  }
+`;
+
+/*   
+    color: white;
+    box-shadow: -3px 6px 2px 1px rgba(0, 0, 255, 0.2);
+   */
+// export const Button = ({ children, onClick }) => {
+//   return (
+//     <button onClick={onClick} className="mybutton">
+//       {children}{" "}
+//     </button>
+//   );
+// };
 
 export default Button;
